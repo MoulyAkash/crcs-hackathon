065a686a5339b9fedcafbcc67aba63e3274cb7c5
MoulyAkash <mouleesh.kumar@gmail.com>
2023-06-16 20:05:12 +0530

    Initial commit

---
+++ src/data/data.jsx
@@ -0,0 +1,538 @@
+export const stateWiseList = [
+  {
+    id: "1",
+    state: "ANDHRA PRADESH",
+    societyNum: "21",
+  },
+  {
+    id: "2",
+    state: "ARUNACHAL PRADESH",
+    societyNum: "1",
+  },
+  {
+    id: "3",
+    state: "ASSAM",
+    societyNum: "6",
+  },
+  {
+    id: "4",
+    state: "BIHAR",
+    societyNum: "19",
+  },
+  {
+    id: "5",
+    state: "CHANDIGARH",
+    societyNum: "1",
+  },
+  {
+    id: "6",
+    state: "CHHATTISGARH",
+    societyNum: "8",
+  },
+  {
+    id: "7",
+    state: "DADRA AND NAGAR HAVELI",
+    societyNum: "1",
+  },
+  {
+    id: "8",
+    state: "GOA",
+    societyNum: "2",
+  },
+  {
+    id: "9",
+    state: "GUJARAT",
+    societyNum: "43",
+  },
+  {
+    id: "10",
+    state: "HARYANA",
+    societyNum: "19",
+  },
+  {
+    id: "11",
+    state: "HIMACHAL PRADESH",
+    societyNum: "1",
+  },
+  {
+    id: "12",
+    state: "JAMMU AND KASHMIR",
+    societyNum: "2",
+  },
+  {
+    id: "13",
+    state: "JHARKHAND",
+    societyNum: "9",
+  },
+  {
+    id: "14",
+    state: "KARNATAKA",
+    societyNum: "28",
+  },
+  {
+    id: "15",
+    state: "KERALA",
+    societyNum: "33",
+  },
+  {
+    id: "16",
+    state: "MADHYA PRADESH",
+    societyNum: "29",
+  },
+  {
+    id: "17",
+    state: "MAHARASHTRA",
+    societyNum: "601",
+  },
+  {
+    id: "18",
+    state: "MANIPUR",
+    societyNum: "4",
+  },
+  {
+    id: "19",
+    state: "NAGALAND",
+    societyNum: "1",
+  },
+  {
+    id: "20",
+    state: "NEW DELHI",
+    societyNum: "39",
+  },
+  {
+    id: "21",
+    state: "ODISHA",
+    societyNum: "31",
+  },
+  {
+    id: "22",
+    state: "PONDICHERRY",
+    societyNum: "1",
+  },
+  {
+    id: "23",
+    state: "PUNJAB",
+    societyNum: "8",
+  },
+  {
+    id: "24",
+    state: "RAJASTHAN",
+    societyNum: "37",
+  },
+  {
+    id: "25",
+    state: "SIKKIM",
+    societyNum: "1",
+  },
+  {
+    id: "26",
+    state: "TAMIL NADU",
+    societyNum: "115",
+  },
+  {
+    id: "27",
+    state: "TELANGANA",
+    societyNum: "29",
+  },
+  {
+    id: "28",
+    state: "TRIPURA",
+    societyNum: "3",
+  },
+  {
+    id: "29",
+    state: "UTTAR PRADESH",
+    societyNum: "120",
+  },
+  {
+    id: "30",
+    state: "UTTARAKHAND",
+    societyNum: "1",
+  },
+  {
+    id: "31",
+    state: "WEST BENGAL",
+    societyNum: "40",
+  },
+];
+
+export const yearWiseList = [
+  {
+    id: "1",
+    year: "1986",
+    societyNum: "1",
+  },
+  {
+    id: "2",
+    year: "1987",
+    societyNum: "1",
+  },
+  {
+    id: "3",
+    year: "1988",
+    societyNum: "4",
+  },
+  {
+    id: "4",
+    year: "1989",
+    societyNum: "3",
+  },
+  {
+    id: "5",
+    year: "1990",
+    societyNum: "2",
+  },
+  {
+    id: "6",
+    year: "1991",
+    societyNum: "10",
+  },
+  {
+    id: "7",
+    year: "1992",
+    societyNum: "8",
+  },
+  {
+    id: "8",
+    year: "1993",
+    societyNum: "8",
+  },
+  {
+    id: "9",
+    year: "1994",
+    societyNum: "12",
+  },
+  {
+    id: "10",
+    year: "1995",
+    societyNum: "7",
+  },
+  {
+    id: "11",
+    year: "1996",
+    societyNum: "7",
+  },
+  {
+    id: "12",
+    year: "1997",
+    societyNum: "8",
+  },
+  {
+    id: "13",
+    year: "1998",
+    societyNum: "4",
+  },
+  {
+    id: "14",
+    year: "1999",
+    societyNum: "20",
+  },
+  {
+    id: "15",
+    year: "2000",
+    societyNum: "15",
+  },
+  {
+    id: "16",
+    year: "2001",
+    societyNum: "20",
+  },
+  {
+    id: "17",
+    year: "2002",
+    societyNum: "15",
+  },
+  {
+    id: "18",
+    year: "2003",
+    societyNum: "16",
+  },
+  {
+    id: "19",
+    year: "2004",
+    societyNum: "35",
+  },
+  {
+    id: "20",
+    year: "2005",
+    societyNum: "28",
+  },
+  {
+    id: "21",
+    year: "2006",
+    societyNum: "15",
+  },
+  {
+    id: "22",
+    year: "2007",
+    societyNum: "16",
+  },
+  {
+    id: "23",
+    year: "2008",
+    societyNum: "31",
+  },
+  {
+    id: "24",
+    year: "2009",
+    societyNum: "33",
+  },
+  {
+    id: "25",
+    year: "2010",
+    societyNum: "54",
+  },
+  {
+    id: "26",
+    year: "2011",
+    societyNum: "95",
+  },
+  {
+    id: "27",
+    year: "2012",
+    societyNum: "247",
+  },
+  {
+    id: "28",
+    year: "2013",
+    societyNum: "194",
+  },
+  {
+    id: "29",
+    year: "2014",
+    societyNum: "250",
+  },
+  {
+    id: "30",
+    year: "2015",
+    societyNum: "83",
+  },
+  {
+    id: "31",
+    year: "2016",
+    societyNum: "19",
+  },
+  {
+    id: "32",
+    year: "2017",
+    societyNum: "7",
+  },
+  {
+    id: "33",
+    year: "2018",
+    societyNum: "8",
+  },
+  {
+    id: "34",
+    year: "2019",
+    societyNum: "9",
+  },
+  {
+    id: "35",
+    year: "2020",
+    societyNum: "10",
+  },
+  {
+    id: "36",
+    year: "2021",
+    societyNum: "13",
+  },
+  {
+    id: "37",
+    year: "2022",
+    societyNum: "67",
+  },
+  {
+    id: "38",
+    year: "2023",
+    societyNum: "26",
+  },
+];
+
+export const financialYearWiseList = [
+  {
+    id: "1",
+    financialYear: "",
+    societyNum: "0",
+  },
+  {
+    id: "2",
+    financialYear: "1899-1900",
+    societyNum: "1",
+  },
+  {
+    id: "3",
+    financialYear: "1986-1987",
+    societyNum: "1",
+  },
+  {
+    id: "4",
+    financialYear: "1987-1988",
+    societyNum: "2",
+  },
+  {
+    id: "5",
+    financialYear: "1988-1989",
+    societyNum: "4",
+  },
+  {
+    id: "6",
+    financialYear: "1989-1990",
+    societyNum: "2",
+  },
+  {
+    id: "7",
+    financialYear: "1990-1991",
+    societyNum: "2",
+  },
+  {
+    id: "8",
+    financialYear: "1991-1992",
+    societyNum: "11",
+  },
+  {
+    id: "9",
+    financialYear: "1992-1993",
+    societyNum: "8",
+  },
+  {
+    id: "10",
+    financialYear: "1993-1994",
+    societyNum: "13",
+  },
+  {
+    id: "11",
+    financialYear: "1994-1995",
+    societyNum: "9",
+  },
+  {
+    id: "12",
+    financialYear: "1995-1996",
+    societyNum: "8",
+  },
+  {
+    id: "13",
+    financialYear: "1996-1997",
+    societyNum: "5",
+  },
+  {
+    id: "14",
+    financialYear: "1997-1998",
+    societyNum: "10",
+  },
+  {
+    id: "15",
+    financialYear: "1998-1999",
+    societyNum: "10",
+  },
+  {
+    id: "16",
+    financialYear: "1999-2000",
+    societyNum: "18",
+  },
+  {
+    id: "17",
+    financialYear: "2000-2001",
+    societyNum: "23",
+  },
+  {
+    id: "18",
+    financialYear: "2001-2002",
+    societyNum: "13",
+  },
+  {
+    id: "19",
+    financialYear: "2002-2003",
+    societyNum: "13",
+  },
+  {
+    id: "20",
+    financialYear: "2003-2004",
+    societyNum: "26",
+  },
+  {
+    id: "21",
+    financialYear: "2004-2005",
+    societyNum: "36",
+  },
+  {
+    id: "22",
+    financialYear: "2005-2006",
+    societyNum: "48",
+  },
+  {
+    id: "23",
+    financialYear: "2006-2007",
+    societyNum: "27",
+  },
+  {
+    id: "24",
+    financialYear: "2007-2008",
+    societyNum: "47",
+  },
+  {
+    id: "25",
+    financialYear: "2008-2009",
+    societyNum: "51",
+  },
+  {
+    id: "26",
+    financialYear: "2009-2010",
+    societyNum: "79",
+  },
+  {
+    id: "27",
+    financialYear: "2010-2011",
+    societyNum: "85",
+  },
+  {
+    id: "28",
+    financialYear: "2011-2012",
+    societyNum: "85",
+  },
+  {
+    id: "29",
+    financialYear: "2012-2013",
+    societyNum: "105",
+  },
+  {
+    id: "30",
+    financialYear: "2013-2014",
+    societyNum: "129",
+  },
+  {
+    id: "31",
+    financialYear: "2014-2015",
+    societyNum: "131",
+  },
+  {
+    id: "32",
+    financialYear: "2015-2016",
+    societyNum: "132",
+  },
+  {
+    id: "33",
+    financialYear: "2016-2017",
+    societyNum: "147",
+  },
+  {
+    id: "34",
+    financialYear: "2017-2018",
+    societyNum: "154",
+  },
+  {
+    id: "35",
+    financialYear: "2018-2019",
+    societyNum: "172",
+  },
+  {
+    id: "36",
+    financialYear: "2019-2020",
+    societyNum: "144",
+  },
+  {
+    id: "37",
+    financialYear: "2020-2021",
+    societyNum: "104",
+  },
+];
